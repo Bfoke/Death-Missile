74618619e34ded0e3455034719c98eeb1460ef34
Brian Fok <bfok@seas.upenn.edu>
2025-09-19 20:53:42 -0400

    kingpin rotate rotates toe link and axle tip around axis

--- Kinematics/SuspensionParts.py
+++ Kinematics/SuspensionParts.py
@@ -58,22 +58,22 @@ class Wishbone:
         # Step 5: Translate back to the original coordinate system
         rotated_point = rotated_translated_point + self.rear
 
-        return rotated_point
+        return rotated_point #upright balljoint location
 
 class Upright:
-    def __init__(self, upper_balljoint, lower_balljoint, toe_root, toe_link, axle_base, axle_tip):
+    def __init__(self, upper_balljoint, lower_balljoint, toe_root, toe_link, axle_root, axle_tip):
         # roots are points along the vector between upper and lower balljoint that the orthogonal projection of toe_link and axle tip go to
-        # they are used to locate the toe_link and axle tip relative to the balljoint vector
+        # they are used to locate the toe_link and axle_tip relative to the balljoint vector
         self.upper_balljoint = upper_balljoint
         self.lower_balljoint = lower_balljoint
         self.toe_root = toe_root
         self.toe_link = toe_link
-        self.axle_base = axle_base
+        self.axle_root = axle_root
         self.axle_tip = axle_tip
-        self.axle_vec = axle_tip - axle_base
+        self.axle_vec = axle_tip - axle_root
         self.joint_dist = np.linalg.norm(upper_balljoint - lower_balljoint)
 
-    def kingpin_rotate(self, upper_bj, lower_bj, theta):
+    def kingpin_rotate(self, theta, upper_bj, lower_bj, toeLink, axleTip):
         vec = upper_bj - lower_bj
         mag = np.linalg.norm(vec)
 
@@ -87,49 +87,61 @@ class Upright:
         sin_t = np.sin(theta)
         one_minus_cos = 1 - cos_t
 
-        # for loop of every critical point in the upright:
-            # apply rotation transformation about unit_vec axis like in control arm class
-        return 1
+        R = np.array([
+            [cos_t + ux**2 * one_minus_cos,
+            ux * uy * one_minus_cos - uz * sin_t,
+            ux * uz * one_minus_cos + uy * sin_t],
+
+            [uy * ux * one_minus_cos + uz * sin_t,
+            cos_t + uy**2 * one_minus_cos,
+            uy * uz * one_minus_cos - ux * sin_t],
+
+            [uz * ux * one_minus_cos - uy * sin_t,
+            uz * uy * one_minus_cos + ux * sin_t,
+            cos_t + uz**2 * one_minus_cos]
+        ])
+
+        rotated_toeLink = R @ (toeLink - lower_bj) + lower_bj
+        rotated_axleTip = R @ (axleTip - lower_bj) + lower_bj
+
+        return rotated_toeLink, rotated_axleTip
     
 class Corner:
     def __init__(self, upper_wb, lower_wb, upright):
-        self.u_wb = upper_wb
-        self.l_wb = lower_wb
-        self.upright = upright
+        self.u_wb = upper_wb #wishbone object
+        self.l_wb = lower_wb #wishbone object
+        self.upright = upright #upright object
 
-    def rotate(self,theta, dTheta=.01):
-        upper_pos = self.u_wb.rotation(theta)
-        dtheta = np.deg2rad(dTheta)
-        max_angle = np.deg2rad(20)
-        steps = int(max_angle/dtheta)
+    def travel(self, theta, steps = 1000, theta_range = 20):
+        upper_pos = self.u_wb.rotation(theta) #rotate upper wishbone
 
-        theta = 0
+        dTheta = np.linspace(0,(theta_range*np.sign(theta)), steps)
+
         theta_close = 0
-        closest_dist = 0
+        closest_dist = float('inf')
         jd = self.upright.joint_dist
 
-        for i in range(steps):
-            lower_pos = self.l_wb.rotation(theta)
+        for dT in dTheta:
+            lower_pos = self.l_wb.rotation(dT)
             dist = np.linalg.norm(upper_pos - lower_pos)
-            theta += dtheta
         
             if abs(dist - jd) < abs(closest_dist - jd):
                 closest_dist = dist
-                theta_close = theta
+                theta_close = dT
 
-        return (self.l_wb.rotation(theta_close))
+        return self.l_wb.rotation(theta_close) #return lower balljoint position that maintains upper/lower balljoint distance
 
 class Rack:
     def __init__(self, right, left, range, rotations):
-        #lets you initialize fucked up angled and/or off center rack but don't do that
+        #lets you make angled and/or off center rack but don't do that
         self.right = right #3d point
         self.left = left #3d point
-        self.range = range #distance a tie rod end moves when going from lock to lock, is equal to 2x the distance from center
+        self.range = range #distance a tie rod end moves when going from lock to lock, is equal to 2x the distance from centered wheel
         self.rotations = rotations
 
     def steer(self, steer_dist):
-        # positive = left turn
-        # negative = right turn
+        # positive steer_dist = left turn
+        # negative steer_dist = right turn
 
         if steer_dist > (self.range/2):
             raise ValueError("steering exceeds rack range")
